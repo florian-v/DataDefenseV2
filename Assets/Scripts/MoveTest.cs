a96d9c51eb540707522b2e211159441daf500352
Narthe <sismikkk@hotmail.fr>
2014-01-23 17:00:35 +0100

    test iTween

---
+++ Assets/Scripts/MoveTest.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		Hashtable options = iTween.Hash(
+			"path", iTweenPath.GetPath ("TestPath"),
+			"time", 5,
+			"easetype", iTween.EaseType.linear
+			);
+		iTween.MoveTo (gameObject, options);
+		//iTween.MoveTo (gameObject, iTween.Hash ("path", iTweenPath.GetPath ("TestPath"), "time", 5));
+	}
+}
